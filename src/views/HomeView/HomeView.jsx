83eb3e9187122aca32b566082217cf04882b1604
alyapolishchuk <alinochkarul081@gmail.com>
2022-08-15 10:02:49 +0300

    c

---
+++ src/views/HomeView/HomeView.jsx
@@ -0,0 +1,27 @@
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+
+export default function HomeView(){
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        gap: '50px',
+        padding: '150px',
+      }}
+    >
+      <Typography variant="h2" component="div">
+        Welcome to PhoneBook!
+      </Typography>
+      <img
+        src="https://cdn.iconscout.com/icon/free/png-512/phone-book-1404933-1187580.png"
+        alt="greeting"
+        width={400}
+      />
+    </Box>
+  );
+}
