7ac203789e0eb885bab8b9fce764f09478701b71
alyapolishchuk <alinochkarul081@gmail.com>
2022-08-15 14:10:23 +0300

    c

--- src/redux/auth/auth-selectors.js
+++
@@ -1,4 +0,0 @@
-export const getToken = state => state.auth.token;
-export const getisLogin = state => state.auth.isLogin;
-export const getUserName = state => state.auth.user.name;
-export const getisRefreshed = state => state.auth.isRefreshed;
