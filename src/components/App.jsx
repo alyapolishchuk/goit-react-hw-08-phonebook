caadebc6c07173859be8d7386353bacc99a5ecc9
alyapolishchuk <alinochkarul081@gmail.com>
2022-08-15 10:30:31 +0300

    c

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,97 @@
 import { Form } from './Form/Form';
 import Section from './Section/Section';
-import ContactList from './ContactList/ContactList';
+// import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
-import { useSelector } from 'react-redux';
-import {
-  errorSelector,
-  loadingSelector,
-} from 'redux/contacts/contacts-selectors';
-import { MutatingDots } from 'react-loader-spinner';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+// import {
+//   errorSelector,
+//   loadingSelector,
+// } from 'redux/contacts/contacts-selectors';
+// import { MutatingDots } from 'react-loader-spinner';
+import { Route, Routes } from 'react-router-dom';
+import { getRefresh } from '../redux/auth/auth-operations';
+import { ContactsView } from '../views/ContactsView/ContactsView';
+import { LoginView } from '../views/LoginView/LoginView';
+import { RegistrationView } from '../views/RegistrationView/RegistrationView';
+import { LayOut } from '../components/Layout/Layout';
+import { getToken } from '../redux/auth/auth-selectors';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import { Box } from '@mui/material';
+import PrivateRoute from '../components/PrivateRoute/PrivateRoute';
+import PublicRoute from '../components/PublicRoute/PublicRoute';
+import HomeView from '../views/HomeView/HomeView';
+import NotFound from '../components/NotFound/NotFound';
+
+
+
 
 
 const App = () => {
-  const customError = useSelector(errorSelector);
-  const loading = useSelector(loadingSelector);
+  const dispatch = useDispatch();
+  const accountToken = useSelector(getToken);
+
+  useEffect(() => {
+    dispatch(getRefresh());
+  }, [dispatch, accountToken]);
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 30,
-        color: '#010101',
-      }}
-    >
-      <Section title="Phonebook">
-        <Form />
-      </Section>
-      <Section title="Contacts">
-        <Filter />
-        {customError && <p>{customError}</p>}
-        {!customError && <ContactList />}
-        {loading && (
-          <div display="block" marginLeft="auto" marginRight="auto">
-            <MutatingDots
-              height="200"
-              width="200"
-              color="#211022"
-              secondaryColor="#00ccff"
-              radius="20"
-              ariaLabel="mutating-dots-loading"
-              wrapperStyle={{}}
-              wrapperClass=""
-              visible={true}
-            />
-          </div>
-        )}
-      </Section>
-    </div>
+    <>
+      <Box
+        sx={{
+          height: '100vh',
+          width: '100vw',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '45px',
+          marginTop: '100px',
+          margin: '0 auto',
+        }}
+      >
+        <Routes>
+          <Route path="/goit-react-hw-08-phonebook/" element={<LayOut />}>
+            <Route index element={<HomeView />} />
+            <Route
+              path="register"
+              element={
+                <PublicRoute>
+                  <RegistrationView />
+                </PublicRoute>
+              }
+            ></Route>
+            <Route
+              path="login"
+              element={
+                <PublicRoute>
+                  <LoginView />
+                </PublicRoute>
+              }
+            ></Route>
+            <Route
+              path="contacts"
+              element={
+                <PrivateRoute>
+                  <ContactsView />
+                </PrivateRoute>
+              }
+            >
+              <Route
+                path="add"
+                element={
+                  <Section title="Add contact">
+                    <Form />
+                  </Section>
+                }
+              ></Route>
+              <Route path="search" element={<Filter />}></Route>
+            </Route>
+          </Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+        <ToastContainer autoClose={2000} theme="colored" />
+      </Box>
+    </>
   );
 };
 
