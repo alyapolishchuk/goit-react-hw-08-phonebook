1e93594b16db4d6e0a0cedae2dcd56c8a0824430
alyapolishchuk <alinochkarul081@gmail.com>
2022-08-15 10:10:08 +0300

    c

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { contactsReducer } from './contacts/contacts-reducers';
+import authReducer from './auth/auth-slice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-//-----------------------------------------------------//
+const persistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
     contacts: contactsReducer,
+    auth: persistedReducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
-export { store };
+const persistor = persistStore(store);
+
+export { persistor, store };
